69f8d606d3e297e727c6878f27cbc3faec7aaf0b
Avil Avate <avilavate123@gmail.com>
2024-07-25 12:56:56 +0000

    react contextapi based localstorage backed todo app

---
+++ todo-context-local/src/App.jsx
@@ -0,0 +1,15 @@
+import 'semantic-ui-css/semantic.min.css'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    </>
+
+  )
+}
+
+export default App
